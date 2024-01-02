0536d0d0b28a4ca2fc6548051bc00f64774b8b9a
Tyler Kuhn <tyler.kuhn94@outlook.com>
2024-01-02 13:00:40 -0500

    fixing POM with best practices

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,6 +1,6 @@
 module.exports = {
     //Fields
-    signupNameField: 'input[placeholder="Name"]', //Works for Signup Page aswell
+    signupNameField: 'input[data-qa="signup-name"]', //Works for Signup Page aswell
     signupEmailField: 'input[data-qa="signup-email"]', //Works for Signup Page aswell
     loginEmailField: 'input[data-qa="login-email"]',
     loginPasswordField: 'input[data-qa="login-password"]',
